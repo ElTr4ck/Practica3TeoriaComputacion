e0ec0a7f7c847ad6e17a6bdd9ddf7b3ef008a0bd
Carlos Razo <carlosrazo2801@hotmail.com>
2023-06-24 14:43:48 -0600

    Programa que pide la expresion

---
+++ Practica3.py
@@ -0,0 +1,12 @@
+#Autores:                                 4CM3
+#Gutierrez Martínez Rodrigo Jozafat
+#Villarreal Razo Carlos Gabriel
+
+def main():
+    print("\t-----------------------------PRACTICA 3-----------------------------\n\n")
+    exp=input(print("Introduce tu expresión en lenguaje C: "))
+
+    return 0
+
+if __name__ == "__main__":
+    main()
