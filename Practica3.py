7bc49e1b0e3dfa00d62f0900282fbfb5ae02575c
Carlos Razo <carlosrazo2801@hotmail.com>
2023-06-25 14:41:46 -0600

    Ya jala full con guión bajo

--- Practica3.py
+++ Practica3.py
@@ -2,14 +2,14 @@
 #Gutierrez Martínez Rodrigo Jozafat
 #Villarreal Razo Carlos Gabriel
 
-#Importacion de la libreria para el maneo de gramaticas
+#----------------------Importacion de la libreria para el maneo de gramaticas----------------------
 import nltk
 from nltk import CFG
-#Importacion de la libreria para la interfaz gráfica
+#----------------------Importacion de la libreria para la interfaz gráfica----------------------
 import tkinter as tk
 from tkinter import messagebox
 
-# Definir la gramática
+#----------------------Definir la gramática----------------------
 gramatica = CFG.fromstring("""
     S -> IDEN '=' ASIG ';'
     IDEN -> LET IDEN | IDEN NUM IDEN | IDEN NUM | LET
@@ -21,7 +21,7 @@ gramatica = CFG.fromstring("""
     OPEN -> IDEN | NUM
 """)
 
-# Definicion de la clase pila para su manejo en la practica
+#----------------------Definicion de la clase pila para su manejo en la practica----------------------
 class Pila:
     def __init__(self):
         self.items = []
@@ -45,29 +45,26 @@ class Pila:
     def tamano(self):
         return len(self.items)
 
-
-#Funcion para identificar si una letra es un simbolo operador
-
+#----------------------Funcion para identificar si una letra es un simbolo operador----------------------
 def isOperador(letra):
     operadores = ['+', '-', '*', '/', '%']
     if letra in operadores: return True
     else: return False
-    
-
-#Funcion del automata completo
+ 
+#----------------------Funcion del automata completo----------------------
 def AutoPila(cadena):
     #Pila para el automata
     pilaAceptacion = Pila()
     estadoActual = 0
     #El primer caracter debe de ser una letra para continuar
-    if cadena[0].isalpha():
+    if cadena[0].isalpha() or cadena[0] == '_':
         for caracter in cadena:
             if estadoActual == 0:
-                if caracter.isalpha() and pilaAceptacion.esta_vacia():
+                if (caracter.isalpha() or caracter == '_') and pilaAceptacion.esta_vacia():
                     estadoActual = 1
                 else: return False
             elif estadoActual == 1:
-                if (caracter.isalpha() or caracter.isdigit()) and ((pilaAceptacion.esta_vacia()) or (pilaAceptacion.cima() == 'I')):
+                if (caracter.isalpha() or caracter.isdigit() or caracter == '_') and ((pilaAceptacion.esta_vacia()) or (pilaAceptacion.cima() == 'I')):
                     estadoActual == 1
                 elif caracter == ';' and (pilaAceptacion.cima() == 'I'):
                     estadoActual = 3
@@ -78,7 +75,7 @@ def AutoPila(cadena):
                     estadoActual = 5
                 else: return False
             elif estadoActual == 2:
-                if caracter.isalpha() and (pilaAceptacion.cima() == 'I'):
+                if (caracter.isalpha() or caracter == '_') and (pilaAceptacion.cima() == 'I'):
                     estadoActual = 1
                 elif caracter.isdigit() and (pilaAceptacion.cima() == 'I'):
                     estadoActual = 4
@@ -105,14 +102,14 @@ def AutoPila(cadena):
             elif estadoActual == 5:
                 if caracter.isdigit() and (pilaAceptacion.cima() == 'I' or pilaAceptacion.cima() == 'P'):
                     estadoActual = 4
-                elif caracter.isalpha() and (pilaAceptacion.cima() == 'I' or pilaAceptacion.cima() == 'P'):
+                elif (caracter.isalpha() or caracter == '_') and (pilaAceptacion.cima() == 'I' or pilaAceptacion.cima() == 'P'):
                     estadoActual = 6
                 elif caracter == '(' and (pilaAceptacion.cima() == 'I' or pilaAceptacion.cima() == 'P'):
                     pilaAceptacion.apilar('P')
                     estadoActual = 5
                 else: return False
             elif estadoActual == 6:
-                if (caracter.isdigit() or caracter.isalpha()) and (pilaAceptacion.cima() == 'I' or pilaAceptacion.cima() == 'P'):
+                if (caracter.isdigit() or caracter.isalpha() or caracter == '_') and (pilaAceptacion.cima() == 'I' or pilaAceptacion.cima() == 'P'):
                     estadoActual = 6
                 elif isOperador(caracter) and (pilaAceptacion.cima() == 'I' or pilaAceptacion.cima() == 'P'):
                     estadoActual = 5
@@ -141,6 +138,7 @@ def AutoPila(cadena):
     else:
         return False
 
+#----------------------Main----------------------
 def ejecutarPrograma():
     #recuperar lo ingresado por el usuario
     exp = entrada.get()
@@ -166,23 +164,23 @@ def ejecutarPrograma():
     else:
         messagebox.showinfo("Alerta", "La cadena NO es aceptada por la gramática")
 
-#Crear la ventana principal
+#----------------------Crear la ventana principal----------------------
 ventana = tk.Tk()
 ventana.title("Practica 3")
-# Configurar el estilo de la ventana
+#----------------------Configurar el estilo de la ventana----------------------
 ventana.configure(bg="#F4F4F4")
 
-# Crear el título
+#----------------------Crear el título----------------------
 titulo = tk.Label(ventana, text="PRACTICA 3", font=("Arial", 18, "bold"), bg="#F4F4F4", fg="#333333")
 titulo.pack(pady=20)
 
-# Crear el campo de entrada
+#----------------------Crear el campo de entrada----------------------
 entrada = tk.Entry(ventana, font=("Arial", 12), bg="#FFFFFF", fg="#333333", relief="solid")
 entrada.pack(pady=10)
 
-# Crear el botón
+#----------------------Crear el botón----------------------
 boton = tk.Button(ventana, text="Comprobar cadena", font=("Arial", 12, "bold"), bg="#4CAF50", fg="#FFFFFF", relief="raised", command=ejecutarPrograma)
 boton.pack(pady=10, padx=50)
 
-#ejecutar la ventana
+#----------------------Ejecutar la ventana----------------------
 ventana.mainloop()
